dd45f98299c03199ade3f7661e3c95314fd28333
Seu Nome <seuemail@dominio.com>
2024-10-22 13:55:54 -0300

    ajustes

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: 'http://localhost:8000',  // O backend rodando na porta 8000
+      target: 'http://localhost:8000/api',  // O backend rodando na porta 8000
       changeOrigin: true,
       pathRewrite: {
         '^/api': '',  // Remove o /api do caminho antes de redirecionar
